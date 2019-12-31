0b2cd3a99141961ec59395b6f3eac3d2e8c60e07
brett <brett.labash@gmail.com>
2019-12-31 10:03:14 -0800

    Add api.ts for getting posts from hackerNews

---
+++ src/utilities/api.ts
@@ -0,0 +1,74 @@
+const api: string = 'https://hacker-news.firebaseio.com/v0';
+const json: string = '.json?print=pretty';
+
+interface Item {
+  id: number;
+  deleted?: boolean;
+  type?: string;
+  by?: string;
+  time: number;
+  text?: string;
+  dead?: boolean;
+  parent?: number;
+  poll?: number;
+  kids?: number[];
+  url?: string;
+  score?: number;
+  title?: string;
+  parts?: number[];
+  descendants?: number | number[];
+}
+
+interface User {
+  id: string;
+  delay?: number;
+  created: number;
+  karma: number;
+  about: string;
+  submitted: number[];
+}
+
+function removeDead(posts: Item[]): Item[] {
+  return posts.filter(Boolean).filter(({ dead }) => dead !== true);
+}
+
+function removeDeleted(posts: Item[]): Item[] {
+  return posts.filter(({ deleted }) => deleted !== true);
+}
+
+function onlyPosts(posts: Item[]): Item[] {
+  return posts.filter(({ type }) => type === 'story');
+}
+
+function onlyComments(posts: Item[]): Item[] {
+  return posts.filter(({ type }) => type === 'comment');
+}
+
+export async function fetchComments(ids: number[]) {
+  const comments = await Promise.all(ids.map(fetchItem));
+  return removeDeleted(onlyComments(removeDead(comments)));
+}
+
+export async function fetchItem(id: number): Promise<Item> {
+  const res = await fetch(`${api}/item/${id}${json}`);
+  return await res.json();
+}
+
+export async function fetchPosts(ids: number[]): Promise<Item[]> {
+  const posts = await Promise.all(ids.map(fetchItem));
+  return removeDeleted(onlyPosts(removeDead(posts)));
+}
+
+export async function fetchUser(id: number): Promise<User> {
+  const user = await fetch(`${api}/user/${id}${json}`);
+  return await user.json();
+}
+
+export async function fetchMainPosts(type: string): Promise<Item[]> {
+  const res = await fetch(`${api}/${type}stories${json}`);
+  const ids = await res.json();
+  if (!ids) throw new Error(`There was an error fetching the ${type} posts.`);
+
+  const posts = await Promise.all<Item>(ids.slice(0, 50).map(fetchItem));
+  return removeDeleted(onlyPosts(removeDead(posts)));
+}
